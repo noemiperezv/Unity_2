da8b9183802cfd1962a021ff8b6135bdf4a81226
noemiperezv <66137245+noemiperezv@users.noreply.github.com>
2022-10-21 13:36:44 -0500

    Reto lección 4

---
+++ Lesson4_Challenge/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Autor: Noemi Pérez Vega
+ * Fecha: 16/10/2022
+ * Descripción: Clase con los métodos para controlar las acciones del enemigo.
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+    private SpawnManagerX scriptSpawnManagerX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+        scriptSpawnManagerX = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>();
+        speed = scriptSpawnManagerX.enemySpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
---
+++ Lesson4_Challenge/Assets/Challenge 4/Scripts/RotateCameraX.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Autor: Noemi Pérez Vega
+ * Fecha: 16/10/2022
+ * Descripción: Clase con la función para controlar el movimiento de la cámara.
+ */using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCameraX : MonoBehaviour
+{
+    private float speed = 200;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime);
+
+        transform.position = player.transform.position; // Move focal point with player
+
+    }
+}
