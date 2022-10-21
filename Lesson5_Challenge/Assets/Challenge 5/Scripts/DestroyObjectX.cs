a6de6947f42b29552a4c499f0ba339df4b94351b
noemiperezv <66137245+noemiperezv@users.noreply.github.com>
2022-10-21 12:32:53 -0500

    Reto lección 5

---
+++ Lesson5_Challenge/Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Autor: Noemi Pérez Vega
+ * Fecha: 20/10/2022
+ * Descripción: Clase para destruir los objetos generados, despues de dos segundos.
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2); // destroy particle after 2 seconds
+    }
+
+
+}
